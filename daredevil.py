c7be0d06771c0831afb040f02dd5bd8483e3460c
Peter Switch <202387707+peter-switch@users.noreply.github.com>
2025-03-29 15:31:38 +0000

    v0.2

--- daredevil.py
+++ daredevil.py
@@ -19,15 +19,29 @@ def filtrar_peligrosos(lista_criminales):
     return peligrosos
 
 # 2. Ordenar de mayor a menor peligrosidad
-def ordenar_peligrosos(lista_peligrosos):
-    print(lista_peligrosos)
-    lista_ordenada=sorted(lista_peligrosos,key=lambda criminal:criminal["peligrosidad"])
+def ordenar_peligrosos(lista_peligrosos): #funcion recibe lista de criminales
+    
+    lista_ordenada=sorted(lista_peligrosos,key=lambda criminal:criminal["peligrosidad"],reverse=True)
+    #sorted ordena y devuelve un listado ordenado
+    #primera parametro:lista,tupla o diccionario
+    #segundo parametro key:en eesete coso una funcion lamdba que recibe cada dato del diccionario y extra la peligrosidad
 
     return lista_ordenada
 
 # 3. Buscar al Kingpin (Wilson Fisk) en la lista
 def encontrar_kingpin(lista_criminales):
-    # ¡Completa aquí!
+
+# ¡Completa aquí!
+    kingpin_encontrado=False
+    for dict in lista_criminales:
+      
+        if dict["nombre"]=="Wilson Fisk":
+            
+            kingpin_encontrado=True
+            break
+        else:    
+            kingpin_encontrado=False
+
     return "¡Kingpin encontrado!" if kingpin_encontrado else "Kingpin no está en la lista."
 
 # 4. Generar mensaje de advertencia
@@ -38,4 +52,5 @@ def mensaje_advertencia(lista_peligrosos):
 
 # --- Ejecución ---
 #print(filtrar_peligrosos(criminales))
-ordenar_peligrosos(criminales)
+#print(ordenar_peligrosos(criminales))
+print(encontrar_kingpin(criminales))
