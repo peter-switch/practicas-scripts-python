f95a759dfe7514de6ab5061e9e14ec0a0ded0f1a
Peter Switch <202387707+peter-switch@users.noreply.github.com>
2025-04-05 10:55:11 +0000

    Ejercicio resulto

--- split-replace-find.py
+++ split-replace-find.py
@@ -11,3 +11,19 @@ Finalmente, une todas las oraciones modificadas y muestra el resultado final.
 
 Cuando hayas completado el ejercicio, comparte tu código y estaré encantado de corregirlo.
 ¡Buena suerte!"""
+
+cadena="La programación en Python es muy divertida y útil. Python es un lenguaje versátil que se utiliza en análisis de datos, desarrollo web, inteligencia artificial y muchas otras áreas. La sintaxis de Python es clara y fácil de aprender, lo que hace que Python sea ideal para principiantes."
+frase_rep=[]
+frases=cadena.split(".")
+
+
+for frase in frases:
+    if frase.find("Python") != -1:
+        frase_rep.append(frase.replace("Python","Python 3"))
+        
+for i in frase_rep:
+   print(f'{i}.', end="")
+
+
+ 
+        
