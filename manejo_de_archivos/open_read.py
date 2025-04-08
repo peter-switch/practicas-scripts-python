61adae03288b83a23144670f7582f6a94f8222b6
Peter Switch <202387707+peter-switch@users.noreply.github.com>
2025-04-08 19:38:46 +0000

    Manejo de archivos

---
+++ manejo_de_archivos/open_read.py
@@ -0,0 +1,4 @@
+nombre_archivo='leeme.txt'
+
+with open(nombre_archivo,'r') as archivo:
+    lineas=archivo.readlines()
--- manejo_de_archivos/open_write.py
+++ manejo_de_archivos/open_write.py
@@ -2,11 +2,13 @@ nombre_archivo='contactos.txt'
 
 
 #Bloque with:una vez ejecutadas las líneas cierra el archivo automáticamente
-with open(nombre_archivo,'w') as archivo:
-    archivo.write('La Maniobra Adama\n')
-    archivo.write('Un podcast muy recomendable\n')
+try:
+    with open(nombre_archivo,'x') as archivo:
+        archivo.write('LODE\n')
+        archivo.write('Un podcast muy recomendable\n')
 
-
+except Exception as e:
+    print(f"HAY UN ERROR: EL ARCHIVO YA EXISTE {e}")
 
 #Método manual: hasta que no se ejecuta close no se cierra el archivo
 archivo=open(nombre_archivo,'a')
